81718b92eef47b60ad602de62c903ae4df902d32
Jonatan Chub <chubjonathan@gmail.com>
2021-05-13 21:09:52 -0600

    Final

---
+++ APIREST/Controllers/InventarioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventarioController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            //Aca estaria la coexion a la base de datos
+            return id switch
+            {
+                1 => "Producto",
+                2 => "Computadora ASUS",
+                _ => throw new NotSupportedException("El id no es valido")
+            };
+
+        }
+
+        public string Post(Producto producto )
+        {
+            //Aca guardamos el producto en la base de datos
+
+            return producto.nombreProducto;
+        }
+
+        public class Producto
+        {
+            public string nombreProducto { get; set; }
+        }
+    }
+}
